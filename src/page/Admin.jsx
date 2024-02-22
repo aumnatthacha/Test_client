181f0cc5ac3ba917ddbdde23613914b100faad87
aumnatthacha <aummumdaeng@gmail.com>
2024-02-22 10:14:49 +0700

    update_navbar

--- src/page/Admin.jsx
+++ src/page/Admin.jsx
@@ -20,7 +20,8 @@ const Admin = () => {
       </div>
 
       {/* navbar */}
-      <div className="w-[1920px] h-[65px] left-0 top-0 absolute bg-white border-b border-stone-300" />
+      {/* <div className="w-[1440px] h-[65px] left-0 top-0 absolute bg-white border-b border-stone-300" /> */}
+      <Navbar/>
 
       {/*  2*/}
 
--- src/page/Navbar.jsx
+++ src/page/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="" className="h-8" alt="" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             w
-          </span>
+          </span> */}
         </a>
         <div className="text-black">Admin</div>
       </div>
